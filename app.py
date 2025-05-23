b833a943b55b6ff3f9fdad45e988bb180f1579b4
larissa-kochinski <larissa.kochinski.carvalho@escola.pr.gov.br>
2025-05-23 07:52:26 -0300

    l

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['POST'])
-def login():
+@app.route('/acesso', methods=['POST'])
+def acesso():
     username = request.form['username']
     password = request.form['password']
     
@@ -18,9 +18,9 @@ def login():
         return "Usuário ou senha incorretos", 401
 
 
-@app.route('/pagina2')
-def pagina2():
-    return render_template('pagina2.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/servicos')
 def servicos():
