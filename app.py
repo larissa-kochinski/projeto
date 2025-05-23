7d3de61a754e1189720f14b641361b425aa28efd
larissa-kochinski <larissa.kochinski.carvalho@escola.pr.gov.br>
2025-05-23 08:12:43 -0300

    l

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
 from flask import Flask, render_template, request, redirect
+import logging
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+
+USERS = {
+    "larissa@": "2004"
+}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html', error=None)
+
 @app.route('/acesso', methods=['POST'])
 def acesso():
     username = request.form['username']
     password = request.form['password']
-    
-    if username == 'larissa@' and password == '2004':
+
+    if username in USERS and USERS[username] == password:
         return redirect('/servicos')
     else:
-        print('Usuário não encontrado')
-        return "Usuário ou senha incorretos", 401
-
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
+        logging.warning(f'Tentativa de login inválido: {username}')
+        return render_template('login.html', error="Usuário ou senha incorretos")
 
 @app.route('/servicos')
 def servicos():
     return render_template('servicos.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
